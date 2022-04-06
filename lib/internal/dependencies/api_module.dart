239633c40a24b1e05496436dd092642a40398153
unknown <teplov0@gmail.com>
2022-04-06 17:54:12 +0500

    add api_module

---
+++ lib/internal/dependencies/api_module.dart
@@ -0,0 +1,8 @@
+import 'package:sunrise/data/api/api_util.dart';
+import 'package:sunrise/data/api/service/sunrise_service.dart';
+
+class ApiModule {
+  static final ApiUtil _apiUtil = ApiUtil(SunriseService());
+
+  static ApiUtil apiUtil() => _apiUtil;
+}
